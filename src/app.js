ef29cb5e11053f0fd3deefa2e98402ab77051ee1
Eliasmalx <Eliasmal0228@gmail.com>
2025-02-05 17:46:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/homero.jpeg";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+window.onload = function generadorDeExcusa() {
+
+  let who = ['Mi primo' , 'Mi sobrino','Mi tia','El vecino','Mi bebe' ];
+  let action = ['quemo','rompio','estropeo','robo','perdio','mojo'];
+  let what = ['mi carro','mi tarea','mi llave','mi ropa','mis gafas'];
+  let when = ['depues de la cena','mientras dormia','durante el concierto','cuando fui a trabajar','cuendo estaba en el trabajo'];
+
+  let excusas = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
+  document.querySelector(".excuse").innerHTML= excusas;
+
+  
 };
